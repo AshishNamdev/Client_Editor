e9dd90ff7ff955bf271364114438e86e2c48776d
Ashish Namdev <ashish28.sirt@gmail.com>
2013-05-09 16:48:32 +0530

    Created env-setup.sh

---
+++ env-setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+src=src/clienteditor
+Target=build/classes/clienteditor
+objects=AgeValidator.class Client.class MaritalStatusConverter.class \
+        EmailValidator.class RequiredStringValidator.class
+
